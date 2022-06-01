7f7f2b73fed307119b0aaed86f69124429247f26
Evgeny Mitin <101546611+MEVpro@users.noreply.github.com>
2022-06-01 17:25:52 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,25 @@ import React from 'react';
 import './App.css';
 import {TasksType, Todolist} from "./Todolist";
 
-let task_1: Array<TasksType> = [
-    { id: 1, title: "Sport", isDone: false},
-    { id: 2, title: "Shopping", isDone: true},
-    { id: 3, title: "Reading", isDone: true}
-]
-let task_2: Array<TasksType> = [
-    { id: 1, title: "CSS", isDone: true},
-    { id: 2, title: "HTML", isDone: true},
-    { id: 3, title: "JS", isDone: false}
-]
-let task_3: Array<TasksType> = [
-    { id: 1, title: "Bread", isDone: true},
-    { id: 2, title: "Eggs", isDone: true},
-    { id: 3, title: "Beer", isDone: false}
-]
 
 function App() {
+
+    const task_1: Array<TasksType> = [
+        { id: 1, title: "Sport", isDone: false},
+        { id: 2, title: "Shopping", isDone: true},
+        { id: 3, title: "Reading", isDone: true}
+    ]
+    const task_2: Array<TasksType> = [
+        { id: 1, title: "CSS", isDone: true},
+        { id: 2, title: "HTML", isDone: true},
+        { id: 3, title: "JS", isDone: false}
+    ]
+    const task_3: Array<TasksType> = [
+        { id: 1, title: "Bread", isDone: true},
+        { id: 2, title: "Eggs", isDone: true},
+        { id: 3, title: "Beer", isDone: false}
+    ]
+
     return (
         <div className="App">
             <Todolist title={"What to do"} tasks={task_1}/>
@@ -27,7 +29,5 @@ function App() {
         </div>
     );
 }
-
-
 
 export default App;
